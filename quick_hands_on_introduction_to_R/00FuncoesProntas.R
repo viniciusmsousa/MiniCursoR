40fdc0a549134fa4461f6ee10c8eb72c5107af47
Vinicius Sousa <vinisousa04@gmail.com>
2019-08-13 18:36:45 -0300

    funcao pronta

---
+++ quick_hands_on_introduction_to_R/00FuncoesProntas.R
@@ -0,0 +1,26 @@
+
+# Selecionar Paises com dados Balanceados ---------------------------------
+
+
+filtro <- function(df){
+  # OBS: 
+  # Seleciona os paises que tem dados balanceados
+  # É necessário que a coluna com os paises tenha o nome de "Pais"
+  
+  lista_paises <- as.character(unique(df$Pais))
+  paises_completos <- vector(mode = "character",length = length(lista_paises))
+  
+  for(i in seq_along(lista_paises)){
+    pais_atual <- df %>% 
+      filter(Pais == lista_paises[i])
+    
+    if(any(is.na(pais_atual))==F){
+      paises_completos[i] <- lista_paises[i]
+    } else {
+      paises_completos[i] <- NA
+    }
+  }
+  dados <- df %>% 
+    filter(Pais %in% paises_completos)
+  return(dados)
+}
